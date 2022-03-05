769c0add2b8d34da95592939cb01017a6d5ffe63
hashedscribe <veray@Veras-shit-computer-2.local>
2022-03-05 17:43:06 -0500

    made a person generator because I cant think of numbers

---
+++ personGenerator.py
@@ -0,0 +1,8 @@
+import random
+from unittest.util import _count_diff_all_purpose
+count = 0
+x = int(input("How many people to generate?"))
+
+while(count < x):
+    print(str(random.randint(0, 15)) + "." + str(random.randint(1,10)) + " " + str(random.randint(10, 100)) + "." + str(random.randint(1,10)) + " " + str(random.randint(0, 30)) + "." + str(random.randint(1,10)))
+    count = count+1
