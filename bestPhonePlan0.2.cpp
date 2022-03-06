4f5a60f6da4c294b299e863ab2f0248b8b606f60
hashedscribe <veray@Veras-shit-computer-2.local>
2022-03-06 12:01:07 -0500

    ranking and bug fixes to algorithm

--- bestPhonePlan0.2.cpp
+++ bestPhonePlan0.2.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <algorithm>
 using namespace std;
 
 class Plan{
@@ -10,18 +11,18 @@ class Plan{
         string company, planName;
         double basePrice, rate, baseMb; //base price (cents and mb)
 
-        double calcx(double tot, bool overcharge){
+        double calcx(double tot){
             tot = tot*1000;
             if(tot < 0){
                 tot = 0;
             }
-            if((tot <= baseMb) && (!overcharge)){
+            if(tot <= baseMb){
                 return basePrice/tot;
             }else{
                 return (basePrice + rate*(tot))/(tot + baseMb);
             }
         }
-        double calcy(double cpmb, bool overcharge){
+        double calcy(double cpmb, double overcharge){
             if(overcharge){
                 return (cpmb * baseMb + basePrice)/rate - cpmb;
             }else{
@@ -30,10 +31,10 @@ class Plan{
         }
         void inspect(bool overcharge){
             if(overcharge){
-                double yint = calcx(0, true);
+                double yint = calcx(0);
                 cout << "With your current usage, you will go over the given data." << endl << endl;
                 cout << "There is no relevant x-intercept." << endl;// x intercept
-                cout << "(" << calcx(0, true) << ", 0) is the y-intercept of the overcharge and is the price of 1 mb there is no overcharge." << endl;// y intercept
+                cout << "(" << calcx(0) << ", 0) is the y-intercept of the overcharge and is the price of 1 mb there is no overcharge." << endl;// y intercept
                 cout << "There is no relevant vertical asymptote." << endl;
                 cout << "y = " << rate << " is the horizontal asymptote. You cannot get more expensive than " << rate << " cents per mb." << endl;// ha
                 //range
@@ -67,6 +68,7 @@ class PersonsPlan{
     public:
         string company, planName;
         double calcx, calcy;
+        
         void setup(string icompany, string iplanName, double icalcx, double icalcy){
             company = icompany;
             planName = iplanName;
@@ -83,10 +85,26 @@ class Person{
         vector <PersonsPlan> deals;
 
         void printPlans(){
+            if(deals.size() == 0){
+                cout << name << " cannot afford any of the plans. Consider using less data or just stop being poor." << endl;
+            }else{
+                for(int i = 0; i < deals.size(); i++){
+                    cout << deals[i].company << "'s " << deals[i].planName << ": " << endl;
+                    cout << "Price per mb (cents): " << deals[i].calcx << endl;
+                    cout << deals[i].company << "'s " << deals[i].planName << " is within " << name << "'s budget and will be considered." << endl << endl;
+                }
+            }
+        }
+        void rank(){
+            PersonsPlan temp;
             for(int i = 0; i < deals.size(); i++){
-                cout << deals[i].company << "'s " << deals[i].planName << ": " << endl;
-                cout << "Price per mb (cents): " << deals[i].calcx << endl;
-                cout << deals[i].company << "'s " << deals[i].planName << " is within " << name << "'s budget and will be considered." << endl << endl;
+                for(int j = 0; j < deals.size(); j++){
+                    if(deals[i].calcx < deals[j].calcx){
+                        temp = deals[j];
+                        deals[j] = deals[i];
+                        deals[i] = temp;
+                    }
+                }
             }
         }
 };
@@ -119,17 +137,18 @@ int main(){
     for(int j = 0; j < numOfPeople; j++){
         for(int i = 0; i < numOfPlans; i++){
             tempAfford = plan[i].afford(person[j].budget, person[j].used);
-            tempCalcx = plan[i].calcx(person[j].used, tempAfford);
-            tempCalcy = plan[i].calcy(person[j].ripoff, tempAfford);
+            tempCalcx = plan[i].calcx(person[j].used);
+            tempCalcy = plan[i].calcy(person[j].ripoff, person[j].used > plan[i].baseMb);
 
             if(tempAfford){ //test if they can afford each plan
                 person[j].deals.push_back(PersonsPlan());
                 person[j].deals[person[j].deals.size()-1].setup(plan[i].company, plan[i].planName, tempCalcx, tempCalcy);
             }
-
         }
     }
     for(int i = 0; i < numOfPeople; i++){
+        person[i].rank();
         person[i].printPlans();
+        cout << "--------------------" << endl;
     }
 }
